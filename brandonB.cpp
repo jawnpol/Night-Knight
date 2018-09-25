defab047c3cb461db479d42cea43b5849dbeebb1
Student <student@lab-240-007.cs.csubak.edu>
2018-09-25 10:52:38 -0700

    BBurdick's Credit

---
+++ brandonB.cpp
@@ -0,0 +1,10 @@
+//Author: Brandon Burdick
+//Night-Knight source code altered
+
+#include<stdio.h>
+#include"fonts.h"
+
+void bb_show_Credit(Rect r)
+{
+    ggprint8b(&r, 16, 0x00ff0000, "Brandon Burdick");
+}

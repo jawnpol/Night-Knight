5df29e6a44f5b71407fbff9aeeab0b9fc90388fa
Student <student@lab-240-007.cs.csubak.edu>
2018-10-02 11:40:37 -0700

    JPC new file

--- johnpaulC.cpp
+++ johnpaulC.cpp
@@ -3,9 +3,53 @@
 
 #include <stdio.h>
 #include "fonts.h"
-
-void jc_show_credits(Rect &r)
+#include <GL/glx.h>
+#include "math.h"
+void jc_show_credits(Rect &r, GLuint texid)
 {
-    ggprint16(&r, 32, 0x00fff000, "John Paul Cailing");	 
+    ggprint16(&r, 320, 0x00fff000, "John Paul Cailing");
+    glColor3ub(255,255,255);
+    int wid = 64;
+
+    static float angle = 0.0f;
+    float fx = (float) ((r.center + r.left) / 2);
+    float fy = (float) (r.center* 1/2);
+
+    fx += sin(angle) * 10.0f;
+    angle += 0.2f;
+    //glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix();
+    glTranslatef(fx,fy,0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+    glEnd();
+    glPopMatrix();   
 }
 
+/*void jpcShowPicture(int x, int y, GLuint texid)
+{
+    glColor3ub(255,255,255);
+    int wid = 64;
+
+    static float angle = 0.0f;
+    float fx = (float) x;
+    float fy = (float) y;
+
+    fx += sin(angle) * 10.0f;
+    angle += 0.2f;
+    //glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix();
+    glTranslatef(fx,fy,0);
+    glBindTexture(GL_TEXTURE_2D, texid);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+    glEnd();
+    glPopMatrix();
+}*/

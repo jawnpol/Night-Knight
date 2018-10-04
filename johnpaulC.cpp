cd69bdda8c222048470ba6b3a19da303b026ac32
Jawnpol <jcailing@csub.edu>
2018-10-04 11:37:29 -0700

    New Sound Changes

--- johnpaulC.cpp
+++ johnpaulC.cpp
@@ -32,25 +32,25 @@ void jc_show_credits(Rect &r)
 
 void jpcShowPicture(int x, int y, GLuint texid)
 {
-    glColor3ub(255,255,255);
-    int wid = 64;
+	glColor3ub(255,255,255);
+	int wid = 64;
 
-    static float angle = 0.0f;
-    float fx = (float) x;
-    float fy = (float) y;
+	static float angle = 0.0f;
+	float fx = (float) x;
+	float fy = (float) y;
 
-    fx += sin(angle) * 10.0f;
-    angle += 0.2f;
-    glPushMatrix();
-    glTranslatef(fx,fy,0);
-    glBindTexture(GL_TEXTURE_2D, texid);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
-    glEnd();
-    glPopMatrix();
+	fx += sin(angle) * 10.0f;
+	angle += 0.2f;
+	glPushMatrix();
+	glTranslatef(fx,fy,0);
+	glBindTexture(GL_TEXTURE_2D, texid);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+	glEnd();
+	glPopMatrix();
 }
 
 //Declaring the buffer and source
@@ -61,22 +61,22 @@ static ALuint gameSong;
 void initSound()
 {
 	//Get started right here.
-    #ifdef USE_OPENAL_SOUND
+	#ifdef USE_OPENAL_SOUND
 	alutInit(0, NULL);
 	if (alGetError() != AL_NO_ERROR) {
 		printf("ERROR: alutInit()\n");
-		return ;
+	return ;
 	}
 	//Clear error state.
 	alGetError();
-	//
+
 	//Setup the listener.
 	//Forward and up vectors are used.
 	float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
 	alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
 	alListenerfv(AL_ORIENTATION, vec);
 	alListenerf(AL_GAIN, 1.0f);
-	
+
 
 	//Buffer holds the sound information.
 	sound  = alutCreateBufferFromFile("./fire.wav");
@@ -96,20 +96,20 @@ void initSound()
 		return ;
 	}
 
-    //Play a looping sound
-    //For future use
+	//Play a looping sound
+	//For future use
 	alSourcef(gameSong, AL_GAIN, 0.5f);
-    alSourcef(gameSong, AL_PITCH, 1.0f);
-    alSourcei(gameSong, AL_LOOPING, AL_TRUE);
-    if (alGetError() != AL_NO_ERROR) {
-        printf("Error: setting source\n");
-        return ;
-    }
+	alSourcef(gameSong, AL_PITCH, 1.0f);
+	alSourcei(gameSong, AL_LOOPING, AL_TRUE);
+	if (alGetError() != AL_NO_ERROR) {
+		printf("Error: setting source\n");
+		return ;
+	}
 }
 
 void cleanupSound()
 {
-    //Cleanup.
+	//Cleanup.
 	//First delete the source.
 	alDeleteSources(1, &alSource);
 	alDeleteSources(1, &gameSong);
@@ -127,22 +127,22 @@ void cleanupSound()
 	alcDestroyContext(Context);
 	//Close device.
 	alcCloseDevice(Device);
-    #endif //USE_OPENAL_SOUND
+	#endif //USE_OPENAL_SOUND
 }
 
 void playSound()
 {
-    #ifdef USE_OPENAL_SOUND
-    alSourcePlay(alSource);
-    #endif //USE_OPENAL_SOUND
+	#ifdef USE_OPENAL_SOUND
+	alSourcePlay(alSource);
+	#endif //USE_OPENAL_SOUND
 }
 
 /*void playGameSound()
 {
 	#ifdef USE_OPENAL_SOUND
-    for (int i = 0; i < 42; i++) {
-    	alSourcePlay(gameSong);
+	for (int i = 0; i < 42; i++) {
+		alSourcePlay(gameSong);
 		usleep(54100000);
-    }
+	}
 	#endif //USE_OPENAL_SOUND
 }*/
